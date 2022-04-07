94c18a4880a6623c9e5f040fb94863dfb95142ba
yoojinlee1145 <yoojin.lee.1145@gmail.com>
2022-04-06 22:17:47 -0500

    Code waits for process and browser auto opens

--- start.ps1
+++ start.ps1
@@ -1,6 +1,9 @@
 $DockerSvc = docker container ls --filter name="mongo" --format '{{.Image}}';
 if ($DockerSvc -ne "mongo") {
-    docker run --name mongo -p 27017:27017 -v caccc-db:/data/db -d --rm mongo;
-    Timeout 1;
+    $job = Start-Job { docker run --name mongo -p 27017:27017 -v caccc-db:/data/db -d --rm mongo; }
+    Wait-Job $job
 }
+ 
+cmd.exe /C start http://Localhost:3000
+ 
 node .\server.js
