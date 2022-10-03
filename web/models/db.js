0b87945eb1890f82cfcba990b662d78cb6b4b5d7
pariah <pariah@pop-os.localdomain>
2022-10-02 20:18:15 -0500

    put the db back

--- web/models/db.js
+++ web/models/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 
 // connect to the local mongoDB at port 27017
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect('mongodb://database:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (!err) { 
         console.log("Mongo started up!") 
     }
