3fb0c589d095ec4c0742b813a0d6ebdda3c4ea70
ericmutton <64441308+ericmutton@users.noreply.github.com>
2022-10-04 20:47:21 -0500

    Fix Powershell script

--- start.ps1
+++ start.ps1
@@ -1,10 +1,10 @@
 # build images if the project image list is empty.
-if (-not (docker-compose -p caccc images -q;)) {
+if (-not { docker-compose -p caccc images -q; }) {
     $job = Start-Job { docker-compose -p caccc build }
     Wait-Job $job
 }
 # start the orchestrated containers if not started.
-if (-not (docker-compose -p caccc ps --filter "status=running" -q)) {
+if (-not { docker-compose -p caccc ps --filter "status=running" -q } ) {
     $job = Start-Job { docker-compose -p caccc up -d }
     Wait-Job $job
 }
