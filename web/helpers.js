13dc5c46d59d9b677cc3e5364118292da82e4b3f
ericmutton <64441308+ericmutton@users.noreply.github.com>
2022-11-17 20:38:37 -0600

    globalize states dict, leverage hbs

--- web/helpers.js
+++ web/helpers.js
@@ -24,6 +24,69 @@ process.resourceTypes = [
     "Senior Services",
     "Transportation",
 ];
+
+const states = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District Of Columbia': 'DC',
+    'Federated States Of Micronesia': 'FM',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Marshall Islands': 'MH',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands': 'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Palau': 'PW',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+};
+
 // for use in the uploads.hbs renderer
 handlebars.registerHelper("downloads", (map, id) => {
     var body = "";
@@ -34,24 +97,12 @@ handlebars.registerHelper("downloads", (map, id) => {
     return new handlebars.SafeString(body);
 });
 // for use in the addOrEdit dropdown
-handlebars.registerHelper("selectedDropDown", (defaultValue, list) => {
-    var body = "";
-    list.forEach(element => {
-        body += "<option value=\"" + handlebars.escapeExpression(element) + "\"";
-        if (element === defaultValue) {
-            body += " selected";
-        }
-        body += ">" + handlebars.escapeExpression(element) + "</option>";
-    });
-    return new handlebars.SafeString(body);
+handlebars.registerHelper("isSelected", (value, selection) => {
+    return value == selection;
 });
 // for use in the mainLayout.hbs dropdown
-handlebars.registerHelper("typesDropdown", () => {
-    var body = "";
-    process.resourceTypes.forEach(element => {
-        body += "<option value=\"" + handlebars.escapeExpression(element) + "\">" + handlebars.escapeExpression(element) + "</option>";
-    });
-    return new handlebars.SafeString(body);
+handlebars.registerHelper("resourceTypes", () => {
+    return process.resourceTypes;
 });
 handlebars.registerHelper("alertFails", (dict, total) => {
     if(!dict) return ""
@@ -66,67 +117,5 @@ handlebars.registerHelper("stateConvert", (state)  =>{
     if (state == null) {
         return new handlebars.SafeString('TX');
     }
-    var states = {
-        'Alabama': 'AL',
-        'Alaska': 'AK',
-        'American Samoa': 'AS',
-        'Arizona': 'AZ',
-        'Arkansas': 'AR',
-        'California': 'CA',
-        'Colorado': 'CO',
-        'Connecticut': 'CT',
-        'Delaware': 'DE',
-        'District Of Columbia': 'DC',
-        'Federated States Of Micronesia': 'FM',
-        'Florida': 'FL',
-        'Georgia': 'GA',
-        'Guam': 'GU',
-        'Hawaii': 'HI',
-        'Idaho': 'ID',
-        'Illinois': 'IL',
-        'Indiana': 'IN',
-        'Iowa': 'IA',
-        'Kansas': 'KS',
-        'Kentucky': 'KY',
-        'Louisiana': 'LA',
-        'Maine': 'ME',
-        'Marshall Islands': 'MH',
-        'Maryland': 'MD',
-        'Massachusetts': 'MA',
-        'Michigan': 'MI',
-        'Minnesota': 'MN',
-        'Mississippi': 'MS',
-        'Missouri': 'MO',
-        'Montana': 'MT',
-        'Nebraska': 'NE',
-        'Nevada': 'NV',
-        'New Hampshire': 'NH',
-        'New Jersey': 'NJ',
-        'New Mexico': 'NM',
-        'New York': 'NY',
-        'North Carolina': 'NC',
-        'North Dakota': 'ND',
-        'Northern Mariana Islands': 'MP',
-        'Ohio': 'OH',
-        'Oklahoma': 'OK',
-        'Oregon': 'OR',
-        'Palau': 'PW',
-        'Pennsylvania': 'PA',
-        'Puerto Rico': 'PR',
-        'Rhode Island': 'RI',
-        'South Carolina': 'SC',
-        'South Dakota': 'SD',
-        'Tennessee': 'TN',
-        'Texas': 'TX',
-        'Utah': 'UT',
-        'Vermont': 'VT',
-        'Virgin Islands': 'VI',
-        'Virginia': 'VA',
-        'Washington': 'WA',
-        'West Virginia': 'WV',
-        'Wisconsin': 'WI',
-        'Wyoming': 'WY'
-        };
     return new handlebars.SafeString(states[state]);
-}
-)
+});
